4c142cbcda82af7d99fcd3bf17ffbe957695e350
Daryna Oliinyk <darusya172@gmail.com>
2019-05-15 14:33:57 +0300

    first commit

---
+++ src/main/java/messaging/App.java
@@ -0,0 +1,31 @@
+package messaging;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class App 
+{
+//    public static void main(String[] args) {
+//
+//    	String str="string";
+////        if (str.length < 1) {
+////            System.err.println("Please provide an input!");
+////            System.exit(0);
+////        }
+//        System.out.println(sha256hex(str));
+//
+//    }
+
+	  public static void main(String[] args) {
+
+	        if (args.length < 1) {
+	            System.err.println("Please provide an input!");
+	            System.exit(0);
+	        }
+	        System.out.println(sha256hex(args[0]));
+
+	    }
+	  
+    public static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
+    }
+}
